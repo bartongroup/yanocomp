23bdb22ac1bb5b7ce72f5f3da8afa4e7ce2326bd
mparker2 <mtparker@dundee.ac.uk>
2021-07-13 11:15:40 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='yanocomp',
-    version='0.1',
+    version='0.2',
     description=(
         'GMM tests for analysis of differential RNA modifications'
     ),
